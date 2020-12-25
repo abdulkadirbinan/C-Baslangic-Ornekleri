42765e392cc33893aadfd0e97bd9b47a9c37a392
abdulkadirbinan <abdulkadirbinan@github.com>
2020-12-25 15:36:34 +0300

    Create ToplamDakikaBulma.c

---
+++ ToplamDakikaBulma.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+double a,b,c;
+printf("gir saat");
+scanf("%lf",&a);
+printf("gir dakika");
+scanf("%lf",&b);
+c=((a*60)+b);
+printf("toplam dakika: %lf",c);
+
+
+
+	return 0;
+}
