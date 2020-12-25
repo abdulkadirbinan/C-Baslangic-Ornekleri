d630ad178f109aeb27d7ff9375ae69d95213d7d2
abdulkadirbinan <abdulkadirbinan@github.com>
2020-12-25 15:27:24 +0300

    BasitCornegi

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main()  {
+
+	int a=10;
+	int b=4;
+	int toplam;
+	int cikarma;
+	int carpma;
+	int bolme;
+	int kalanibul;
+	
+	toplam = a + b ;
+	cikarma = a - b ;
+	carpma = a * b ;
+	bolme = a / b ;
+	kalanibul = a % b ;
+	
+	printf("toplam %d \n " , toplam );
+	printf("cikarma %d \n" , cikarma );
+	printf("carpma %d \n " , carpma );
+    printf("bolme %d \n " , bolme );
+	printf("kalanibul %d \n " , kalanibul);
+		
+
+
+
+       
+
+}
